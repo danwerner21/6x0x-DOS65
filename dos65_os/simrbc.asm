0d0abd043f426a192ebbe7c55c93e73db88806d6
Dan Werner <danwerner21@gmail.com>
2024-06-29 22:17:12 +0000

    duodyne updates

--- dos65_os/simrbc.asm
+++ dos65_os/simrbc.asm
@@ -135,6 +135,22 @@ boot:
 
 setup:
         LDX     #0              ;clear index
+;
+        LDA     #<cnstxt        ; STORE POINTER TO COMMAND LINE
+        STA     cmdlnp
+        LDA     #>cnstxt
+        STA     cmdlnp + 1
+
+        LDA     #<dskcfg        ; STORE POINTER TO DISK CONFIG TABLE FOR APPS
+        STA     dskcfpc
+        LDA     #>dskcfg
+        STA     dskcfpc + 1
+
+        LDA     #<dcba          ; STORE POINTER TO DCB TABLES FOR APPS
+        STA     dcbpc
+        LDA     #>dcba
+        STA     dcbpc + 1
+;
 ;first clear key dba variables
 ;        STX     hstact          ;host buffer inactive
 ;        STX     unacnt          ;clear unalloc count
